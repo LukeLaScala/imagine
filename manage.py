656f622c752b1e4c84eedabaff143c3510ca8a37
Luke LaScala <lascala.luke@gmail.com>
2017-11-26 19:11:20 -0500

    Fixed manage.py

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_script import Manager, Server
-from ctflearn import app
+from lhd import app
 
 manager = Manager(app)
 manager.add_command("runserver", Server(host='0.0.0.0', port=8000))
